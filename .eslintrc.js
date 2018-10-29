2e4e98289a0d4276addd398ac1364c9eee153ffa
Suresh Murali <m.suresh.fx@gmail.com>
2018-10-28 19:28:09 -0700

    Cleaned create react app

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+    "node": true, 
+    "browser": true,
+    "es6": true
+    },
+    "rules": {
+    "react/jsx-filename-extension": "off",
+    "react/prefer-stateless-function": "off",
+    "global-require": "off",
+    "no-console": "off",
+    "react/forbid-prop-types": "off",
+    "no-param-reassign": "off",
+    "consistent-return": "off",
+    "react/no-unused-state": "off"
+    },
+    "extends": "airbnb"
+    };
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#### Suresh Murali Portfolio website
+# Suresh Murali Portfolio website
 
 ![WEBSITE UNDER CONSTRUCTION](http://www.benanova.com/Site/themed_images/in_construction/in_construction1.jpg
 )
