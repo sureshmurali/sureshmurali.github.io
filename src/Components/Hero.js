d464c31febcb37b282d3d54c34cbcbfc23d5edfc
Suresh Murali <m.suresh.fx@gmail.com>
2018-10-30 22:38:03 -0700

    Added fonts and global style

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -2,16 +2,37 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const MyName = styled.div`
-  font-size: 100px;
+  font-family: 'Valencia';
+  font-size: 150px;
   text-align:center;
+`;
+
+const MyPosition = styled.div`
+  font-family: 'AvenirRoman';
+  font-size: 50px;
+  text-align:center;
+`;
+const Container = styled.div`
+    display: flex;
+    flex-flow: column nowrap;
+    border: 1px dashed red;
+    justify-content: center;
+    align-items: center;
+    height:100vh;
 `;
 
 class Hero extends Component {
   render() {
     return (
-      <MyName>
-        SURESH MURALI
-      </MyName>
+      <Container>
+        <MyName>
+            Suresh Murali
+        </MyName>
+        <br />
+        <MyPosition>
+          {'UI/UX designer & Front end developer'}
+        </MyPosition>
+      </Container>
     );
   }
 }
