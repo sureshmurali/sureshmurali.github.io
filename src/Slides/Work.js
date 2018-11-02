1f2df215cc640e3fc2e8038257baae0e8ba9a8d3
Suresh Murali <m.suresh.fx@gmail.com>
2018-11-01 21:55:17 -0700

    Static work details added

--- src/Slides/Work.js
+++ src/Slides/Work.js
@@ -1,27 +1,76 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-const MyName = styled.div`
-  font-family: 'Valencia';
-  font-size: 150px;
-  text-align:center;
+const ProjectName = styled.div`
+  font-family: 'AvenirHeavy';
+  font-size: 100px;
+  /* border: 1px dashed black; */
 `;
+
+const ProjectDesc = styled.div`
+  padding-top:2%;
+  font-family: 'AvenirBook';
+  font-size: 30px;
+  /* border: 1px dashed black; */
+`;
+
+const MyRole = styled.div`
+  padding-top:5%;
+  font-family: 'AvenirMedium';
+  font-size: 30px;
+  /* border: 1px dashed black; */
+`;
+
+const ProjectID = styled.div`
+  font-family: 'AvenirHeavy';
+  font-size: 30px;
+  /* border: 1px dashed black; */
+  padding: 5%;
+`;
+
 const Container = styled.div`
     display: flex;
-    flex-flow: column nowrap;
-    border: 1px dashed #EEE;
-    justify-content: center;
-    align-items: center;
+    flex-flow: row nowrap;
+    /* border: 1px dashed red; */
     height:100vh;
+`;
+
+const TextContainer = styled.div`
+display: flex;
+flex-flow: column nowrap;
+/* border: 1px dashed black; */
+height:100vh;
+width: 55%;
+`;
+
+const ProjectDetails = styled.div`
+display: flex;
+flex-flow: column nowrap;
+/* border: 2px solid black; */
+padding-left:20%;
+padding-top:10%;
 `;
 
 class Work extends Component {
   render() {
     return (
       <Container>
-        <MyName>
-            Suresh Murali
-        </MyName>
+        <TextContainer>
+          <ProjectID>
+            01
+          </ProjectID>
+          <ProjectDetails>
+            <ProjectName>
+            Voistrap
+            </ProjectName>
+            <ProjectDesc>
+            IoT project to give workplace insights using indoor localization, voice and schedule.
+            </ProjectDesc>
+            <MyRole>
+              UI Designer •  Front end dev
+            </MyRole>
+          </ProjectDetails>
+        </TextContainer>
       </Container>
     );
   }
