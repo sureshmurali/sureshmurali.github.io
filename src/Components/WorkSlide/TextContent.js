657bcb4440d98b07dfa484eb6a3216570d80d2b3
Suresh Murali <m.suresh.fx@gmail.com>
2018-11-04 14:34:09 -0800

    New component for static text content in text slide

---
+++ src/Components/WorkSlide/TextContent.js
@@ -0,0 +1,139 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const ProjectName = styled.div`
+  font-family: 'AvenirHeavy';
+  font-size: 100px;
+  /* border: 1px dashed black; */
+`;
+
+const ProjectDesc = styled.div`
+  padding-top:2%;
+  font-family: 'AvenirBook';
+  font-size: 30px;
+  /* border: 1px dashed black; */
+`;
+
+const MyRole = styled.div`
+  padding-top:5%;
+  font-family: 'AvenirMedium';
+  font-size: 30px;
+  /* border: 1px dashed black; */
+`;
+
+const ProjectID = styled.div`
+  font-family: 'AvenirHeavy';
+  font-size: 35px;
+  /* border: 1px dashed black; */
+  padding: 5%;
+`;
+
+const ProjectType = styled.div`
+  font-family: 'AvenirHeavy';
+  font-size: 30px;
+  /* border: 1px dashed black; */
+  padding: 5%;
+`;
+
+const TextContainer = styled.div`
+display: flex;
+flex-flow: column nowrap;
+/* border: 1px dashed black; */
+height:100vh;
+width: 50%;
+`;
+
+const ProjectDetails = styled.div`
+display: flex;
+flex-flow: column nowrap;
+/* border: 2px solid black; */
+`;
+
+const EmptyBlock = styled.div`
+height:30%; /** NEED RESPO */
+/* border: 2px solid black; */
+`;
+
+const ProjectDetailsContainer = styled.div`
+display: flex;
+justify-content:center;
+flex-flow: column nowrap;
+align-items: center;
+/* border: 2px solid black; */
+padding-left:20%;
+height: 100%;
+`;
+
+class TextContent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      reveal: false,
+    };
+    this.revealText = this.revealText.bind(this);
+  }
+
+  componentDidMount() {
+    const { timeDelay } = this.props;
+    this.revealText(timeDelay);
+  }
+
+  revealText(timeout) {
+    setTimeout(() => {
+      this.setState({ reveal: true });
+    }, timeout);
+  }
+
+  render() {
+    const {
+      number, projectName, projectDesc, roles, projectType,
+    } = this.props;
+    return (
+      <TextContainer>
+        <ProjectID>
+          {number}
+        </ProjectID>
+        <ProjectDetailsContainer>
+          <ProjectDetails>
+            <ProjectName>
+              {projectName}
+            </ProjectName>
+            <ProjectDesc>
+              {projectDesc}
+            </ProjectDesc>
+            <MyRole>
+              {
+                    roles.map((role, index, arr) => (index === arr.length - 1 ? (
+                      <span>
+                        {role}
+                      </span>
+                    ) : (
+                      <span>
+                        {role}
+                          &nbsp; • &nbsp;
+                      </span>
+                    )))
+                }
+            </MyRole>
+          </ProjectDetails>
+          <EmptyBlock />
+        </ProjectDetailsContainer>
+
+        <ProjectType>
+          {projectType}
+        </ProjectType>
+      </TextContainer>
+    );
+  }
+}
+
+TextContent.propTypes = {
+  number: PropTypes.string.isRequired,
+  projectName: PropTypes.string.isRequired,
+  projectDesc: PropTypes.string.isRequired,
+  projectType: PropTypes.number.isRequired,
+  roles: PropTypes.array.isRequired,
+};
+
+export default TextContent;
--- src/Slides/Work.js
+++ src/Slides/Work.js
@@ -1,39 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-
-const ProjectName = styled.div`
-  font-family: 'AvenirHeavy';
-  font-size: 100px;
-  /* border: 1px dashed black; */
-`;
-
-const ProjectDesc = styled.div`
-  padding-top:2%;
-  font-family: 'AvenirBook';
-  font-size: 30px;
-  /* border: 1px dashed black; */
-`;
-
-const MyRole = styled.div`
-  padding-top:5%;
-  font-family: 'AvenirMedium';
-  font-size: 30px;
-  /* border: 1px dashed black; */
-`;
-
-const ProjectID = styled.div`
-  font-family: 'AvenirHeavy';
-  font-size: 35px;
-  /* border: 1px dashed black; */
-  padding: 5%;
-`;
-
-const ProjectType = styled.div`
-  font-family: 'AvenirHeavy';
-  font-size: 30px;
-  /* border: 1px dashed black; */
-  padding: 5%;
-`;
+import TextContent from '../Components/WorkSlide/TextContent';
 
 const Container = styled.div`
     display: flex;
@@ -42,62 +9,17 @@ const Container = styled.div`
     height:100vh;
 `;
 
-const TextContainer = styled.div`
-display: flex;
-flex-flow: column nowrap;
-/* border: 1px dashed black; */
-height:100vh;
-width: 50%;
-`;
-
-const ProjectDetails = styled.div`
-display: flex;
-flex-flow: column nowrap;
-/* border: 2px solid black; */
-`;
-
-const EmptyBlock = styled.div`
-height:30%; /** NEED RESPO */
-/* border: 2px solid black; */
-`;
-
-const ProjectDetailsContainer = styled.div`
-display: flex;
-justify-content:center;
-flex-flow: column nowrap;
-align-items: center;
-/* border: 2px solid black; */
-padding-left:20%;
-height: 100%;
-`;
-
 class Work extends Component {
   render() {
     return (
       <Container>
-        <TextContainer>
-          <ProjectID>
-            01
-          </ProjectID>
-          <ProjectDetailsContainer>
-            <ProjectDetails>
-              <ProjectName>
-              Voistrap
-              </ProjectName>
-              <ProjectDesc>
-              IoT project to give workplace insights using indoor localization, voice and schedule.
-              </ProjectDesc>
-              <MyRole>
-              UI Designer&nbsp; • &nbsp;Full stack developer
-              </MyRole>
-            </ProjectDetails>
-            <EmptyBlock />
-          </ProjectDetailsContainer>
-
-          <ProjectType>
-            WEB APP
-          </ProjectType>
-        </TextContainer>
+        <TextContent
+          number="01"
+          projectName="Voistrap"
+          projectDesc="IoT project to give workplace insights using indoor localization, voice and schedule."
+          projectType="WEB APP"
+          roles={['UI designer', 'Front-end developer']}
+        />
       </Container>
     );
   }
