904f255a7547d2c5d70f530f7af65713a96595bc
Suresh Murali <m.suresh.fx@gmail.com>
2018-11-06 19:07:16 -0800

    Gets scroll distance and screen height inside class method

--- src/App.js
+++ src/App.js
@@ -6,11 +6,35 @@ import Work from './Slides/Work';
 import './Assets/index.css';
 
 const GlobalStyle = createGlobalStyle`
-html, body { margin: 0; }
+html, body { margin: 0;}
 *, *:before, *:after { box-sizing: border-box; }
 `;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      vh: 0,
+    };
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+    this.setState({ vh: Math.round(window.innerHeight) });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll(event) {
+    const { body, documentElement } = event.srcElement;
+    const { vh } = this.state;
+    console.log(Math.max(body.scrollTop, documentElement.scrollTop));
+    console.log('VH: ', vh);
+  }
+
   render() {
     return (
       <React.Fragment>
