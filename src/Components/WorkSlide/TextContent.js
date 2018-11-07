0262650361d1032419cea4315927dad85ad20d23
Suresh Murali <m.suresh.fx@gmail.com>
2018-11-07 14:42:07 -0800

    Fixed Text content layout

--- src/Components/WorkSlide/TextContent.js
+++ src/Components/WorkSlide/TextContent.js
@@ -5,41 +5,45 @@ import PropTypes from 'prop-types';
 const ProjectName = styled.div`
   font-family: 'AvenirHeavy';
   font-size: 100px;
-  /* border: 1px dashed black; */
+  border: 1px dashed black;
 `;
 
 const ProjectDesc = styled.div`
   padding-top:2%;
   font-family: 'AvenirBook';
   font-size: 30px;
-  /* border: 1px dashed black; */
+  min-height: 150px; /** NEED RESPO */
+  border: 1px dashed black;
 `;
 
 const MyRole = styled.div`
   padding-top:5%;
   font-family: 'AvenirMedium';
   font-size: 30px;
-  /* border: 1px dashed black; */
+  border: 1px dashed black;
 `;
 
 const ProjectID = styled.div`
   font-family: 'AvenirHeavy';
   font-size: 35px;
-  /* border: 1px dashed black; */
+  border: 1px dashed black;
   padding: 5%;
 `;
 
 const ProjectType = styled.div`
   font-family: 'AvenirHeavy';
   font-size: 30px;
-  /* border: 1px dashed black; */
+  border: 1px dashed black;
   padding: 5%;
 `;
 
 const TextContainer = styled.div`
+position: fixed;
+top:0;
+left:0;
 display: flex;
 flex-flow: column nowrap;
-/* border: 1px dashed black; */
+border: 1px dashed black;
 height:100vh;
 width: 50%;
 `;
@@ -47,13 +51,11 @@ width: 50%;
 const ProjectDetails = styled.div`
 display: flex;
 flex-flow: column nowrap;
-/* border: 1px dashed black; */
+border: 1px dashed black;
+width: 100%;
+padding-left:20%;
 `;
 
-const EmptyBlock = styled.div`
-height:30%; /** NEED RESPO */
-/* border: 2px solid black; */
-`;
 
 const ProjectDetailsContainer = styled.div`
 display: flex;
@@ -61,30 +63,11 @@ justify-content:center;
 flex-flow: column nowrap;
 align-items: center;
 /* border: 2px solid black; */
-padding-left:20%;
+
 height: 100%;
 `;
 
 class TextContent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      reveal: false,
-    };
-    this.revealText = this.revealText.bind(this);
-  }
-
-  componentDidMount() {
-    const { timeDelay } = this.props;
-    this.revealText(timeDelay);
-  }
-
-  revealText(timeout) {
-    setTimeout(() => {
-      this.setState({ reveal: true });
-    }, timeout);
-  }
-
   render() {
     const {
       number, projectName, projectDesc, roles, projectType,
@@ -110,13 +93,11 @@ class TextContent extends Component {
               ) : (
                 <span key={role}>
                   {role}
-                  {' '}
                         &nbsp; • &nbsp;
                 </span>
               )))}
             </MyRole>
           </ProjectDetails>
-          <EmptyBlock />
         </ProjectDetailsContainer>
 
         <ProjectType>
