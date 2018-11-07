3df3b38611c131361d7a2d0eba4d06edcd40c923
Suresh Murali <m.suresh.fx@gmail.com>
2018-11-07 14:41:45 -0800

    Moved work details to constructor and added duplicates

--- src/Slides/Work.js
+++ src/Slides/Work.js
@@ -18,36 +18,7 @@ class Work extends Component {
       slideNumber: 0,
     };
     this.handleScroll = this.handleScroll.bind(this);
-  }
-
-  componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
-    this.setState({ vh: Math.round(window.innerHeight) });
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
-  }
-
-  handleScroll(event) {
-    const { body, documentElement } = event.srcElement;
-    const { vh, slideNumber } = this.state;
-    let scrollDistance = Math.max(body.scrollTop, documentElement.scrollTop);
-    if (Math.floor(scrollDistance / vh) > 1) {
-      scrollDistance /= 2;
-    }
-    if (Math.floor(scrollDistance / vh) !== slideNumber && slideNumber < 3) {
-      console.log(Math.floor(scrollDistance / vh));
-      this.setState({ slideNumber: Math.floor(scrollDistance / vh) });
-    } else if (slideNumber === 3 && (Math.floor(scrollDistance / vh) < slideNumber)) {
-      console.log(Math.floor(scrollDistance / vh));
-      this.setState({ slideNumber: Math.floor(scrollDistance / vh) });
-    }
-  }
-
-  changeTextContentBasedOnScroll() {
-    const { slideNumber } = this.state;
-    const workDetails = [
+    this.workDetails = [
       {
         number: '',
         projectName: '',
@@ -59,7 +30,7 @@ class Work extends Component {
         number: '01',
         projectName: 'Voistrap',
         projectDesc: 'IoT project to give workplace insights using indoor localization, voice and schedule.',
-        projectType: 'WEB APP',
+        projectType: 'iOS & WEB APP',
         roles: ['UI designer', 'Full Stack developer'],
       },
       {
@@ -76,14 +47,79 @@ class Work extends Component {
         projectType: 'iOS APP',
         roles: ['UI designer', 'Front-end developer'],
       },
+      {
+        number: '04',
+        projectName: 'Tesla',
+        projectDesc: 'iOS app to remember your fav food at each restaurant you eat..',
+        projectType: 'iOS APP',
+        roles: ['UI designer', 'Front-end developer'],
+      },
+      {
+        number: '05',
+        projectName: 'Wakanda',
+        projectDesc: 'iOS app to remember your fav food at each restaurant you eat..',
+        projectType: 'iOS APP',
+        roles: ['UI designer', 'Front-end developer'],
+      },
+      {
+        number: '06',
+        projectName: 'Insta bot',
+        projectDesc: 'iOS app to remember your fav food at each restaurant you eat..',
+        projectType: 'iOS APP',
+        roles: ['UI designer', 'Front-end developer'],
+      },
+      {
+        number: '',
+        projectName: '',
+        projectDesc: '',
+        projectType: '',
+        roles: [''],
+      },
     ];
+    console.log(this.workDetails);
+    let offset = 0;
+    this.workDetails.forEach((ele, ind, arr) => {
+      if (ind >= 1) {
+        arr.splice(ind + offset, 0, arr[ind + offset]);
+        offset = ind;
+      }
+    });
+    console.log(this.workDetails);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+    this.setState({ vh: Math.round(window.innerHeight) });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll(event) {
+    const { body, documentElement } = event.srcElement;
+    const { vh, slideNumber } = this.state;
+    const scrollDistance = Math.max(body.scrollTop, documentElement.scrollTop);
+    if (Math.floor(scrollDistance / vh) !== slideNumber
+      && slideNumber < this.workDetails.length - 1) {
+      console.log(Math.floor(scrollDistance / vh));
+      this.setState({ slideNumber: Math.floor(scrollDistance / vh) });
+    } else if (slideNumber === this.workDetails.length - 1
+      && (Math.floor(scrollDistance / vh) < slideNumber)) {
+      console.log(Math.floor(scrollDistance / vh));
+      this.setState({ slideNumber: Math.floor(scrollDistance / vh) });
+    }
+  }
+
+  changeTextContentBasedOnScroll() {
+    const { slideNumber } = this.state;
     return (
       <TextContent
-        number={workDetails[slideNumber].number}
-        projectName={workDetails[slideNumber].projectName}
-        projectDesc={workDetails[slideNumber].projectDesc}
-        projectType={workDetails[slideNumber].projectType}
-        roles={workDetails[slideNumber].roles}
+        number={this.workDetails[slideNumber].number}
+        projectName={this.workDetails[slideNumber].projectName}
+        projectDesc={this.workDetails[slideNumber].projectDesc}
+        projectType={this.workDetails[slideNumber].projectType}
+        roles={this.workDetails[slideNumber].roles}
       />
     );
   }
