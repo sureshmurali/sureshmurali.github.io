222ecbb9ed5f5b56ae2bcb7b38a16d15ed43a31d
Suresh Murali <m.suresh.fx@gmail.com>
2018-11-06 18:27:59 -0800

    Updated Work slide with rest of the projects

--- src/Slides/Work.js
+++ src/Slides/Work.js
@@ -12,15 +12,35 @@ const Container = styled.div`
 class Work extends Component {
   render() {
     return (
-      <Container>
-        <TextContent
-          number="01"
-          projectName="Voistrap"
-          projectDesc="IoT project to give workplace insights using indoor localization, voice and schedule."
-          projectType="WEB APP"
-          roles={['UI designer', 'Front-end developer']}
-        />
-      </Container>
+      <React.Fragment>
+        <Container>
+          <TextContent
+            number="01"
+            projectName="Voistrap"
+            projectDesc="IoT project to give workplace insights using indoor localization, voice and schedule."
+            projectType="WEB APP"
+            roles={['UI designer', 'Full stack developer']}
+          />
+        </Container>
+        <Container>
+          <TextContent
+            number="02"
+            projectName="ComingOrNot"
+            projectDesc="Event planner web app that strives to ease the work of an organizer, conduct events and get togethers in a much planned and less chaotic way."
+            projectType="WEB APP"
+            roles={['UI designer', 'Front-end developer']}
+          />
+        </Container>
+        <Container>
+          <TextContent
+            number="03"
+            projectName="WhatsMyFood"
+            projectDesc="iOS app to remember your fav food at each restaurant you eat.."
+            projectType="iOS APP"
+            roles={['UI designer', 'Front-end developer']}
+          />
+        </Container>
+      </React.Fragment>
     );
   }
 }
