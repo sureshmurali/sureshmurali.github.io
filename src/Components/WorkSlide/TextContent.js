adf112cd32b857aa372b836be385370b16eb844c
Suresh Murali <m.suresh.fx@gmail.com>
2018-11-07 17:10:20 -0800

    Moved TextContainer to top

--- src/Components/WorkSlide/TextContent.js
+++ src/Components/WorkSlide/TextContent.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
+const TextContainer = styled.div`
+position: fixed;
+top:0;
+left:0;
+display: flex;
+flex-flow: column nowrap;
+/* border: 1px dashed black; */
+height:100vh;
+width: 50%;
+`;
+
 const ProjectName = styled.div`
   font-family: 'AvenirHeavy';
   font-size: 100px;
-  border: 1px dashed black;
+  /* border: 1px dashed black; */
 `;
 
 const ProjectDesc = styled.div`
@@ -13,45 +24,34 @@ const ProjectDesc = styled.div`
   font-family: 'AvenirBook';
   font-size: 30px;
   min-height: 150px; /** NEED RESPO */
-  border: 1px dashed black;
+  /* border: 1px dashed black; */
 `;
 
 const MyRole = styled.div`
   padding-top:5%;
   font-family: 'AvenirMedium';
   font-size: 30px;
-  border: 1px dashed black;
+  /* border: 1px dashed black; */
 `;
 
 const ProjectID = styled.div`
   font-family: 'AvenirHeavy';
   font-size: 35px;
-  border: 1px dashed black;
+  /* border: 1px dashed black; */
   padding: 5%;
 `;
 
 const ProjectType = styled.div`
   font-family: 'AvenirHeavy';
   font-size: 30px;
-  border: 1px dashed black;
+  /* border: 1px dashed black; */
   padding: 5%;
-`;
-
-const TextContainer = styled.div`
-position: fixed;
-top:0;
-left:0;
-display: flex;
-flex-flow: column nowrap;
-border: 1px dashed black;
-height:100vh;
-width: 50%;
 `;
 
 const ProjectDetails = styled.div`
 display: flex;
 flex-flow: column nowrap;
-border: 1px dashed black;
+/* border: 1px dashed black; */
 width: 100%;
 padding-left:20%;
 `;
