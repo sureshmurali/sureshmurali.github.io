a89ca7fac1fe99d8f0052b8f07501f58cbbf4a9b
Suresh Murali <m.suresh.fx@gmail.com>
2018-11-07 17:08:51 -0800

    Fixed mobile layout

--- src/Components/WorkSlide/ImageContent.js
+++ src/Components/WorkSlide/ImageContent.js
@@ -17,34 +17,22 @@ max-height: 90vh;
 `;
 
 const ImageBox = styled.div`
-margin-top:98vh;
 border: 1px dashed green;
-margin-bottom:2vh;
+height: 100vh;
+position: fixed;
+top:0;
+right:0;
+width: 50%;
+z-index:-1;
 display: flex;
 justify-content: center;
 align-items: center;
-height: 100vh;
 `;
 
 class ImageContent extends Component {
   render() {
     return (
       <ImageContainer>
-        <ImageBox>
-          <PhoneImage src={voistrapImg} alt="voistrapMap" />
-        </ImageBox>
-        <ImageBox>
-          <PhoneImage src={voistrapImg} alt="voistrapMap" />
-        </ImageBox>
-        <ImageBox>
-          <PhoneImage src={voistrapImg} alt="voistrapMap" />
-        </ImageBox>
-        <ImageBox>
-          <PhoneImage src={voistrapImg} alt="voistrapMap" />
-        </ImageBox>
-        <ImageBox>
-          <PhoneImage src={voistrapImg} alt="voistrapMap" />
-        </ImageBox>
         <ImageBox>
           <PhoneImage src={voistrapImg} alt="voistrapMap" />
         </ImageBox>
