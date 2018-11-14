3af5308fb95699025e25c9ac53cff4d4a3ef52fa
Suresh Murali <m.suresh.fx@gmail.com>
2018-11-14 15:55:41 -0800

    About me text added

--- src/Slides/Hero.js
+++ src/Slides/Hero.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import TextReveal from '../Components/HeroSlide/TextReveal';
 
-const Container = styled.div`
+const NameAndJobTitle = styled.div`
     display: flex;
     flex-flow: column nowrap;
     justify-content: center;
@@ -10,17 +10,36 @@ const Container = styled.div`
     height:100vh;
     width:100%;
     background-color: white;
-    margin-bottom: 40vh; 
 `;/* Since pageSplitTime is 1.4 */
+
+const AboutMe = styled.div`
+    height: 40vh;
+    width:100%;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: center;
+    font-size: 40px;
+    font-family: 'AvenirRoman';
+    text-align: center;
+    transform: translateY(40%);
+`;
 
 class Hero extends Component {
   render() {
     return (
-      <Container>
-        <TextReveal text="Suresh Murali" fontFam="Valencia" fontSizeInPx="150" timeDelay={500} />
-        <br />
-        <TextReveal text="UI/UX Designer & Front-end Developer" fontFam="AvenirRoman" fontSizeInPx="50" timeDelay={1300} />
-      </Container>
+      <React.Fragment>
+        <NameAndJobTitle>
+          <TextReveal text="Suresh Murali" fontFam="Valencia" fontSizeInPx="150" timeDelay={500} />
+          <br />
+          <TextReveal text="UI/UX Designer & Front-end Developer" fontFam="AvenirRoman" fontSizeInPx="50" timeDelay={1300} />
+        </NameAndJobTitle>
+        <AboutMe>
+        I’m a front-end developer who cares deeply about user experience.
+          <br />
+        I have serious passion for UI design and new technologies.
+        </AboutMe>
+      </React.Fragment>
     );
   }
 }
