2dbf20d05513c2d81ed174d3b9cf450fc1695207
Suresh Murali <m.suresh.fx@gmail.com>
2019-01-05 17:58:45 +0900

    Use client height instead of inner height

--- src/Slides/Mobile/WorkSlide/Work.js
+++ src/Slides/Mobile/WorkSlide/Work.js
@@ -82,7 +82,9 @@ class Work extends Component {
 
   componentDidMount() {
     window.addEventListener('scroll', this.handleScroll);
-    this.setState({ vh: Math.round(window.innerHeight * this.pageSplitTimes) });
+    this.setState(
+      { vh: Math.round(window.document.documentElement.clientHeight * this.pageSplitTimes) },
+    );
   }
 
   componentWillUnmount() {
