a2bd02011d26229babcbcdfa86b57cef01b2706b
Suresh Murali <suresh.murali29@gmail.com>
2025-05-23 20:19:57 +0900

    Change build output from docs to dist and use relative paths for assets

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   root: 'src',
+  base: './',
   build: {
-    outDir: '../docs',
+    outDir: '../dist',
   },
   server: {
     open: true
