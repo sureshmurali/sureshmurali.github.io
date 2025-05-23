e50bb1fb513edf6f7ce33dad6529ae5b8102aa7b
Suresh Murali <suresh.murali29@gmail.com>
2025-05-23 21:10:58 +0900

    Updates

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+    "node": true, 
+    "browser": true,
+    "es6": true
+    },
+    "parser": "babel-eslint",
+    "rules": {
+    "react/jsx-filename-extension": "off",
+    "react/prefer-stateless-function": "off",
+    "global-require": "off",
+    "no-console": "off",
+    "react/forbid-prop-types": "off",
+    "no-param-reassign": "off",
+    "consistent-return": "off",
+    "react/no-unused-state": "off"
+    },
+    "extends": "airbnb"
+    };
