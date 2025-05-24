9cb46f9ecbc041e9f03ef2fbbbbe16a2f77b5353
Suresh Murali <suresh.murali29@gmail.com>
2025-05-24 14:30:12 +0900

    refactor: Improve TextContent.js with better formatting and code structure

--- src/Slides/WideScreen/WorkSlide/TextContent.js
+++ src/Slides/WideScreen/WorkSlide/TextContent.js
@@ -4,14 +4,14 @@ import PropTypes from 'prop-types';
 import device from '../../../Assets/Responsive/breakpoints';
 
 const TextContainer = styled.section`
-position: fixed;
-top:0;
-left:0;
-display: flex;
-flex-flow: column nowrap;
-/** border: 5px dashed black; */
-height:100vh;
-width: 50%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-flow: column nowrap;
+  /** border: 5px dashed black; */
+  height: 100vh;
+  width: 50%;
 `;
 
 const ProjectName = styled.div`
@@ -89,46 +89,46 @@ const ProjectType = styled.div`
 `;
 
 const ProjectDetails = styled.div`
-display: flex;
-flex-flow: column nowrap;
-/* border: 1px dashed black; */
-width: 100%;
-padding: 5%;
-padding-left:10%;
+  display: flex;
+  flex-flow: column nowrap;
+  /* border: 1px dashed black; */
+  width: 100%;
+  padding: 5%;
+  padding-left: 10%;
 `;
 
 
 const ProjectDetailsContainer = styled.div`
-display: flex;
-flex-flow: column nowrap;
-align-items: center;
-/* border: 2px solid black; */
-padding-top:5%;
-height: 100%;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  /* border: 2px solid black; */
+  padding-top: 5%;
+  height: 100%;
 `;
 
 const appearText = () => keyframes`
-0%{
-  color: #FFF;
-}
-100%{
-  color: #333;
-}
+  0% {
+    color: #FFF;
+  }
+  100% {
+    color: #333;
+  }
 `;
 
 const revBlock = () => keyframes`
-0%{
+  0% {
     left: 0;
-    width: 0%
-}
-50%{
-    left:0%;
-    width:100%
-}
-100%{
-    left:100%;
-    width:0%
-}
+    width: 0%;
+  }
+  50% {
+    left: 0%;
+    width: 100%;
+  }
+  100% {
+    left: 100%;
+    width: 0%;
+  }
 `;
 
 
@@ -136,25 +136,23 @@ let BlockTextReveal = styled.span`
 `;
 
 const BlockTextRevealQuick = styled.span`
-display:${props => (props.inline ? 'inline' : 'block')};
-color: #FFF;
-animation: ${appearText} 0.0001s linear forwards;
-animation-delay: 0.5s;
-position: relative;
+  display: ${props => (props.inline ? 'inline' : 'block')};
+  color: #FFF;
+  animation: ${appearText} 0.0001s linear forwards;
+  animation-delay: 0.5s;
+  position: relative;
 
-
-
-&::after{
-content:'';
-top:0;
-left:0;
-position:absolute;
-width:0%;
-height:100%;
-background: #222;
-animation: ${revBlock} 1s cubic-bezier(0.19, 1, 0.22, 1) forwards;
-animation-delay:0s;
-}
+  &::after {
+    content: '';
+    top: 0;
+    left: 0;
+    position: absolute;
+    width: 0%;
+    height: 100%;
+    background: #222;
+    animation: ${revBlock} 1s cubic-bezier(0.19, 1, 0.22, 1) forwards;
+    animation-delay: 0s;
+  }
 `;
 
 const BlockTextRevealNoAnim = styled.span`
@@ -176,13 +174,18 @@ class TextContent extends Component {
 
   refresh(nextProps) {
     const { refreshToggle } = nextProps;
+    
+    // Only trigger animation when refreshToggle is true
     if (refreshToggle) {
+      // First set to non-animated version to reset
       BlockTextReveal = BlockTextRevealNoAnim;
-      this.setState({ refreshBlock: true },
-        () => {
-          BlockTextReveal = BlockTextRevealQuick;
-          this.setState({ refreshBlock: false });
-        });
+      
+      // Set refreshBlock state and then update BlockTextReveal in callback
+      this.setState({ refreshBlock: true }, () => {
+        // Switch to animated version to play animation
+        BlockTextReveal = BlockTextRevealQuick;
+        this.setState({ refreshBlock: false });
+      });
     }
   }
 
@@ -190,6 +193,22 @@ class TextContent extends Component {
     const {
       number, projectName, projectDesc, roles, projectType, refreshToggle,
     } = this.props;
+    
+    // Helper function to render roles with bullet separators
+    const renderRoles = (roleList) => {
+      return roleList.map((role, index, arr) => {
+        const isLastRole = index === arr.length - 1;
+        
+        return isLastRole ? (
+          <span key={role}>{role}</span>
+        ) : (
+          <span key={role}>
+            {role}&nbsp; • &nbsp;
+          </span>
+        );
+      });
+    };
+    
     return (
       <TextContainer>
         <ProjectID>
@@ -197,6 +216,7 @@ class TextContent extends Component {
             {number}
           </BlockTextReveal>
         </ProjectID>
+        
         <ProjectDetailsContainer>
           <ProjectDetails>
             <ProjectName>
@@ -204,20 +224,13 @@ class TextContent extends Component {
                 {projectName}
               </BlockTextReveal>
             </ProjectName>
+            
             <MyRole>
               <BlockTextReveal refreshToggle={refreshToggle} inline>
-                {roles.map((role, index, arr) => (index === arr.length - 1 ? (
-                  <span key={role}>
-                    {role}
-                  </span>
-                ) : (
-                  <span key={role}>
-                    {role}
-                        &nbsp; • &nbsp;
-                  </span>
-                )))}
+                {renderRoles(roles)}
               </BlockTextReveal>
             </MyRole>
+            
             <ProjectDesc>
               <BlockTextReveal refreshToggle={refreshToggle} inline={false}>
                 {projectDesc}
